05d2c32d8cd1ef55fcfb7b845dab141b6e5b45cb
Vitalii Zavadskyi <zavadskyi@jinr.ru>
2025-12-09 18:01:06 +0300

    fix: __init__

--- src/dgm_fit/__init__.py
+++ src/dgm_fit/__init__.py
@@ -2,10 +2,13 @@ from importlib.metadata import version
 
 __version__ = version(__name__)
 
-from .dataset import Dataset, get_path_data
+from .fit_result import FitResult
+from .iminuit_minimizer import IMinuitMinimizer
+from .minimizable import Minimizable
 
 __all__ = [
-    "Dataset",
-    "get_path_data",
+    "FitResult",
+    "IMinuitMinimizer",
+    "Minimizable",
     "__version__",
 ]
